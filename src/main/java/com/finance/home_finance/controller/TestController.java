e7ec62e6df888775b4b060493f8d57dff0530eaf
Sergey Vlasov <madk1nd86@gmail.com>
2019-11-10 21:40:57 +0200

    Add default spring boot template

---
+++ src/main/java/com/finance/home_finance/controller/TestController.java
@@ -0,0 +1,15 @@
+package com.finance.home_finance.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @GetMapping("/test")
+    public ResponseEntity<String> test() {
+        return new ResponseEntity<>("OK", HttpStatus.ACCEPTED);
+    }
+}
