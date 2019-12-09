6cc3e86e22b17f1670a3f086772cb5349351691f
Sergey Vlasov <sergey.vlasov@devexperts.com>
2019-12-09 23:24:32 +0200

    Extract variables for asserted values

--- backend-api/src/test/java/com/finance/home_finance/controller/ColorControllerTests.java
+++ backend-api/src/test/java/com/finance/home_finance/controller/ColorControllerTests.java
@@ -25,10 +25,16 @@ class ColorControllerTests {
     @Autowired
     private MockMvc mvc;
 
+    private String code1;
+    private String code2;
+
     @BeforeEach
     void init() {
-        service.save(new ColorDTO(UUID.randomUUID(), "#000000"));
-        service.save(new ColorDTO(UUID.randomUUID(), "#FFFFFF"));
+        code1 = "#000000";
+        code2 = "#FFFFFF";
+
+        service.save(new ColorDTO(UUID.randomUUID(), code1));
+        service.save(new ColorDTO(UUID.randomUUID(), code2));
     }
 
     @Test
@@ -37,7 +43,7 @@ class ColorControllerTests {
 //                .andDo(print())
                 .andExpect(status().isAccepted())
                 .andExpect(jsonPath("$.length()").value(2))
-                .andExpect(jsonPath("$[0].code").value("#000000"))
-                .andExpect(jsonPath("$[1].code").value("#FFFFFF"));
+                .andExpect(jsonPath("$[0].code").value(code1))
+                .andExpect(jsonPath("$[1].code").value(code2));
     }
 }
